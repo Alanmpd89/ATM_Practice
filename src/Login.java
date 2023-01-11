b64d7be13f34e3b0cc3fd9a029563a71fa8641e7
Alanmpd89 <Alanmpd89@gmail.com>
2023-01-11 11:07:00 -0500

    Login setup

---
+++ src/Login.java
@@ -0,0 +1,12 @@
+public class Login {
+
+    public static void main(String[] args) {
+
+        System.out.println("Log in page");
+
+        System.out.println("Enter your email and password");
+
+
+    }
+
+}
