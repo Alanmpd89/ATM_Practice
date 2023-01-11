2afc3abf86886400c8f76d0ee05337d58fe074f9
Alanmpd89 <Alanmpd89@gmail.com>
2023-01-11 11:09:35 -0500

    Balance page

---
+++ src/Balance.java
@@ -0,0 +1,13 @@
+public class Balance {
+
+    public static void main(String[] args) {
+
+        System.out.println("Your balance is");
+
+        double total = 500000;
+
+        System.out.println(total);
+
+
+    }
+}
